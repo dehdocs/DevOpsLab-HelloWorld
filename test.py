2baf1e768396b48b4506899da2bb6e7e34524f6c
dehdocs <dehdocs@gmail.com>
2018-08-06 22:23:20 -0400

    devops course

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "devops test!a.")
+        self.assertRegex(result.data.decode(), "andre")
 
 
 if __name__ == "__main__":
